71471eb2682a2f8e696f656958912e0910541e6d
Cuong Ho <cuong.ict.10101@gmail.com>
2017-05-21 15:32:45 +0700

    Add lint

--- src/sum.js
+++ src/sum.js
@@ -1,40 +1,41 @@
 function calculator(testStr) {
-   var testArr = testStr.split(',');
-   var sum = 0;
-   var i = 0, j = 0;
-   var numbers = [];
+  const testArr = testStr.split(',');
+  let sum = 0;
+  let i = 0;
+  let j = 0;
+  let numbers = [];
 
-   if (typeof testStr !== 'string') {
-     return false;
-   }
-   if (testStr.trim().length === 0) {
-     return 0;
-   }
-   if (testArr.length > 2) {
-     return false;
-   }
-
-
-   for (i; i< testArr.length; i++) {
-     var temp = testArr[i].split('\n');
-     numbers = numbers.concat(temp);
-   }
-
-   for (j; j < numbers.length; j++) {
-      var num;
-      try {
-        num = parseInt(numbers[j]);
-        if (num < 0) {
-          return false;
-        } else if (num > 1000) {
-          continue;
-        }
-        sum += num;
-      } catch(err) {
-        return false;
-      }  
+  if (typeof testStr !== 'string') {
+    return false;
+  }
+  if (testStr.trim().length === 0) {
+    return 0;
+  }
+  if (testArr.length > 2) {
+    return false;
   }
 
-   return sum;
+
+  for (i; i < testArr.length; i += 1) {
+    const temp = testArr[i].split('\n');
+    numbers = numbers.concat(temp);
+  }
+
+  for (j; j < numbers.length; j += 1) {
+    let num;
+    try {
+      num = parseInt(numbers[j], 10);
+      if (num < 0) {
+        return false;
+      } else if (num < 1000) {
+        sum += num;
+      }
+    } catch (err) {
+      return false;
+    }
+  }
+
+  return sum;
 }
 
+calculator('2,3');
